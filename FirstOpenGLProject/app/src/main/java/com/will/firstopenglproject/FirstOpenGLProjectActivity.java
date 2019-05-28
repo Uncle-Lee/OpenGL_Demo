48abc4f5ecc27b09352dfc5509e772b73d5843e0
Jie Li <jie.li@ihandysoft.com>
2019-05-28 10:35:02 +0800

    添加Render，创建GLSurfaceview显示在屏幕上

--- FirstOpenGLProject/app/src/main/java/com/will/firstopenglproject/FirstOpenGLProjectActivity.java
+++ FirstOpenGLProject/app/src/main/java/com/will/firstopenglproject/FirstOpenGLProjectActivity.java
@@ -1,13 +1,38 @@
 package com.will.firstopenglproject;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FirstOpenGLProjectActivity extends AppCompatActivity {
 	
+	private GLSurfaceView mGLSurfaceView;
+	private boolean rendererSet = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		mGLSurfaceView = new GLSurfaceView(this);
+		mGLSurfaceView.setEGLContextClientVersion(2);
+		mGLSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+		rendererSet = true;
+		setContentView(mGLSurfaceView);
 	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (rendererSet) {
+			mGLSurfaceView.onResume();
+		}
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (rendererSet) {
+			mGLSurfaceView.onPause();
+		}
+	}
+	
 }
