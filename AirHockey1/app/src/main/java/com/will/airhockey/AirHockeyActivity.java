34f4eccf9818cd7d93b22d531321a9b9d0bf8450
Jie Li <jie.li@ihandysoft.com>
2019-05-28 11:44:01 +0800

    定义顶点和着色器

---
+++ AirHockey1/app/src/main/java/com/will/airhockey/AirHockeyActivity.java
@@ -0,0 +1,38 @@
+package com.will.airhockey;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AirHockeyActivity extends AppCompatActivity {
+	
+	private GLSurfaceView mGLSurfaceView;
+	private boolean rendererSet = false;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mGLSurfaceView = new GLSurfaceView(this);
+		mGLSurfaceView.setEGLContextClientVersion(2);
+		mGLSurfaceView.setRenderer(new AirHockeyRenderer());
+		rendererSet = true;
+		setContentView(mGLSurfaceView);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (rendererSet) {
+			mGLSurfaceView.onResume();
+		}
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (rendererSet) {
+			mGLSurfaceView.onPause();
+		}
+	}
+	
+}
---
+++ AirHockey1/app/src/main/res/raw/simple_vertex_shader.glsl
@@ -0,0 +1,7 @@
+#version 120
+
+attribute vec4 a_Position;
+
+void main() {
+    gl_Position = a_Position;
+}
