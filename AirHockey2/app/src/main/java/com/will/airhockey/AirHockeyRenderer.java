df81a6fdaf45de9d74a0eccac6da52433f1c82a1
jie.li <jie.li@ihandysoft.com>
2019-06-16 15:59:54 +0800

    为每个定点添加颜色属性

--- AirHockey2/app/src/main/java/com/will/airhockey/AirHockeyRenderer.java
+++ AirHockey2/app/src/main/java/com/will/airhockey/AirHockeyRenderer.java
@@ -24,6 +24,7 @@ import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glUniform4f;
@@ -36,15 +37,19 @@ import static android.opengl.GLES20.glViewport;
  */
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 	
+	private static final String A_COLOR = "a_Color";
 	private static final String U_COLOR = "u_Color";
 	private static final String A_POSITION = "a_Position";
 	private int uColorLocation;
 	private int aPositionLocation;
 	private static final int POSITION_COMPONENT_COUNT = 2;
-	private static final  int BYTES_PER_FLOAT = 4;
+	private static final int COLOR_COMPONENT_COUNT = 3;
+	private static final int BYTES_PER_FLOAT = 4;
+	private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 	private final FloatBuffer vertexData;
 	private final Context context;
 	private int program;
+	private int aColorLocation;
 	
 	public AirHockeyRenderer(Context context) {
 		/*float[] tableVertices = {
@@ -99,14 +104,18 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 		}
 		
 		glUseProgram(program);
-		
-		uColorLocation = glGetUniformLocation(program, U_COLOR);
+
+		aColorLocation = glGetAttribLocation(program, A_COLOR);
 		aPositionLocation = glGetAttribLocation(program, A_POSITION);
 		
 		vertexData.position(0);  // 从内存区域的开头开始读值
 		// 告诉OpenGL去哪里读数据
-		glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, vertexData);
-		GLES20.glEnableVertexAttribArray(aPositionLocation);
+		glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+		glEnableVertexAttribArray(aPositionLocation);
+
+		vertexData.position(POSITION_COMPONENT_COUNT);
+		glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+		glEnableVertexAttribArray(aColorLocation);
 	}
 	
 	@Override
@@ -119,17 +128,17 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		// 画桌子
-		glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+		//glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
 		glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 		
 		// 画桌子中心分割线
-		GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-		GLES20.glDrawArrays(GL_LINES, 6, 2);
+		//glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_LINES, 6, 2);
 		
 		// 画木槌
-		GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-		GLES20.glDrawArrays(GL_POINTS, 8, 1);
-		glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+		//glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_POINTS, 8, 1);
+		//glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
 		glDrawArrays(GL_POINTS, 9, 1);
 	}
 }
